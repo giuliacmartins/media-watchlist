ed5d667cadf243348748076d169d28a8c377d8ec
giulia <116525917+giuliacmartins@users.noreply.github.com>
2023-12-24 01:35:25 -0500

    Delete watchlist/mylist/static/loginpopup.js

--- watchlist/mylist/static/loginpopup.js
+++
@@ -1,6 +0,0 @@
-const content_section = document.querySelector('.content-section');
-const loginPopUp = document.querySelector('.popup-login-btn');
-
-loginPopUp.addEventListener('click', ()=> {
-    content_section.classList.add('active-popup');
-});
